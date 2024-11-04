2d5b17c943ef9a9a5300815cc3c937977ea318d7
Shreyansh <Shreyanshsri2812@gmail.com>
2024-11-04 18:05:33 +0530

    Navbar created

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import "./App.css";
 import Navbar from "./components/shared/Navbar";
+import Sidebar from "./components/Sidebar";
 
 function App() {
 
 
   return (
-    <div>
+    <div className="bg-[#F6F8FC] h-screen w-screen overflow-hidden">
       <Navbar/>
+      <Sidebar/>
     </div>
       
     
--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,22 +1,53 @@
-import React from 'react'
+import React from "react";
+import Avatar from "react-avatar";
+import { CiCircleQuestion, CiSettings } from "react-icons/ci";
+import { IoIosSearch } from "react-icons/io";
+import { PiDotsNineBold } from "react-icons/pi";
 import { RxHamburgerMenu } from "react-icons/rx";
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between mx-3 h-16'>
-        <div className='flex items-center gap-10'>
-            <div className='flex items-center gap-2'>
-                <div className='p-3 rounded-full hover:bg-gray-100 cursor-pointer'>
-                <RxHamburgerMenu size={"20px"}/>
-                </div>
-                <img className='w-8' src="https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png" alt="gmail-logo" />
-                <h1 className='text-2xl text-gray-500 font-medium'> Gmail</h1>
-            </div>
-
+    <div className="flex items-center justify-between mx-3 h-16">
+      <div className="flex items-center gap-10">
+        <div className="flex items-center gap-2">
+          <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+            <RxHamburgerMenu size={"20px"} />
+          </div>
+          <img
+            className="w-8"
+            src="https://mailmeteor.com/logos/assets/PNG/Gmail_Logo_512px.png"
+            alt="gmail-logo"
+          />
+          <h1 className="text-2xl text-gray-500 font-medium"> Gmail</h1>
         </div>
-
+      </div>
+      <div className="md:block hidden w-[50%] mr-60">
+        <div className="flex items-center bg-[#EAF1FB] px-2 py-3 rounded-full">
+          <IoIosSearch size={"24px"} className="text-gray-700"/>
+          <input 
+          type="text"
+          placeholder="Search  Mail"
+          className="rounded-full w-full bg-transparent outline-none px-1" />
+        </div>
+      </div>
+      <div className="md:block hidden">
+        <div className="flex items-center gap-2">
+            <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+                <CiCircleQuestion size={"20px"}/>
+            </div>
+            <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+                <CiSettings size={"20px"}/>
+            </div>
+            <div className="p-3 rounded-full hover:bg-gray-100 cursor-pointer">
+                <PiDotsNineBold size={"20px"}/>
+            </div>
+            <div className="cursor-pointer">
+                <Avatar src="https://cdn-icons-png.flaticon.com/512/2919/2919906.png" size="40" round={true} />
+            </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
